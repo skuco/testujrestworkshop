e03e87888ca6887f5c02a43d45bf4807f4b6fe8b
skuco <Str@toc@ster1>
2020-04-29 21:54:08 +0200

    added homework tests

--- src/test/java/tests/SpellsTest.java
+++ src/test/java/tests/SpellsTest.java
@@ -1,14 +1,16 @@
 package tests;
 
+import groovy.lang.GString;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 
-import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -43,14 +45,15 @@ public class SpellsTest {
 
     @Test
     void itShouldReturnSpellForEachSpellInList() {
-        List<HashMap<Object , Object>> spells =
-        when().get()
+        List<HashMap<Object,Object>> spells = when().get()
                 .then().extract().response()
                 .jsonPath().getList("$");
+
         spells.forEach(spell -> {
             assertThat(spell.get("effect").toString() , not(emptyOrNullString()));
             assertThat(spell.get("spell").toString() , not(emptyOrNullString()));
             assertThat(spell.get("id").toString() , not(emptyOrNullString()));
+            //System.out.println(spell.get("spell").toString());
         });
     }
 
@@ -62,5 +65,29 @@ public class SpellsTest {
         System.out.println(size);
         assertThat(size, is(151));
 
+    }
+
+    @Test
+    void itShouldContainOnlyCertainSpells() {
+        List<String> desiredSpells = Arrays.asList("Imperio","Avada Kedavra","Crucio");
+        List<String> spells = when().get()
+                .then().statusLine(containsString("OK"))
+                .extract().response()
+                .jsonPath().getList("spell");
+
+        for (String spell : spells) {
+            for (String desiredSpell : desiredSpells) {
+                if (spell.equals(desiredSpell)) {
+                    System.out.println("Spell " + desiredSpell + " found.");
+                }
+            }
+        }
+    }
+
+    @Test
+    void checkStatusLine() {
+        given().pathParam("spellId", "5b74ee1d3228320021ab6239")
+                .when().get("/{spellId}")
+                .then().statusLine(containsString("OK"));
     }
 }
